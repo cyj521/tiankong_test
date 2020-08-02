5a1ffa82a2417df4c3ec998400923be543a11c73
cyj521 <67511142+cyj521@users.noreply.github.com>
2020-08-02 20:09:09 +0800

    Add files via upload

---
+++ 复习.py
@@ -0,0 +1,14 @@
+# a=[1,2,3,4]
+# b=(1,)
+# c={1:"a"}
+# if isinstance(a,list):
+#     print(a)
+# import re
+# string="lihndhlincjdli"
+# res=re.findall(r"li",string)
+
+a={1:2}
+b={1:2}
+print(a is b)
+print(a==b)
+
